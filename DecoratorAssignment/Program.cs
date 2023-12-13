40781bb26f3da5a1f4ab7c0d93d368e8e43b8dd9
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:21:35 +0200

    Update Program.cs
    Add Program.cs

--- DecoratorAssignment/Program.cs
+++ DecoratorAssignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DecoratorAssignment
+﻿using static DecoratorAssignment.Program;
+
+namespace DecoratorAssignment
 {
     // Instructions
     // You can implement your whole solution here
@@ -7,11 +9,98 @@
     // Chapeter 3 the DecoratorPattern: Decorating Objects (starts at page 79)
     // Link to pdf: https://github.com/ajitpal/BookBank/blob/master/%5BO%60Reilly.%20Head%20First%5D%20-%20Head%20First%20Design%20Patterns%20-%20%5BFreeman%5D.pdf
     // NOTE: Remember that the code examples in this book are written in java so you can't just copy the code examples given there
-    internal class Program
+
+    //This pattern attaches additional responsibilities to an object dynamically.Decorators provide a flexible alternative to subcalssing for extending functionality.
+    public class Program
     {
+
+        public abstract class Beverage
+        {
+            protected string description = "No coffee";
+            public string GetDescription() => description;
+            public abstract decimal Cost();
+
+        }
+
+        public class HouseBlend : Beverage
+        {
+            public HouseBlend() { description = "House Blend Coffee"; }
+            public override decimal Cost() => 0.89M;
+        }
+        public class DarkRoast : Beverage
+        {
+            public DarkRoast() { description = "Dark Roast Coffee"; }
+            public override decimal Cost() => 0.99M;
+        }
+        public class Decaf : Beverage
+        {
+            public Decaf() { description = "Decaf Coffee"; }
+            public override decimal Cost() => 1.05M;
+        }
+
+        public class Espresso : Beverage
+        {
+            public Espresso() { description = "Espresso"; }
+            public override decimal Cost() => 1.99M;
+        }
+
+        // Create Decorator class
+        public abstract class Condiment : Beverage
+        {
+            public Beverage beverage; // Reference to the beverage to wrap. 
+        }
+
+        public class Milk : Condiment
+        {
+            public Milk(Beverage beverage)
+            {
+                this.beverage = beverage;
+                description = beverage.GetDescription() + ", Milk";
+            }
+            public override decimal Cost() => beverage.Cost() + 0.20M;
+
+        }
+
+        public class Mocha : Condiment
+        {
+            public Mocha(Beverage beverage)
+            {
+                this.beverage = beverage;
+                description = beverage.GetDescription() + ", Mocha";
+            }
+
+            public override decimal Cost() => beverage.Cost() + 0.20M;
+        }
+        public class Soy : Condiment
+        {
+            public Soy(Beverage beverage)
+            {
+                this.beverage = beverage;
+                description = beverage.GetDescription() + ", Soy";
+            }
+            public override decimal Cost() => beverage.Cost() + 0.15M;
+
+        }
+        public class Whip : Condiment
+        {
+            public Whip(Beverage beverage)
+            {
+                this.beverage = beverage;
+                description = beverage.GetDescription() + ", Whip";
+            }
+
+            public override decimal Cost() => beverage.Cost() + 0.10M;
+
+        }
         static void Main(string[] args)
         {
-            
+
+            Beverage beverage = new Decaf();
+            beverage = new Mocha(beverage);
+            beverage = new Whip(beverage);
+            Console.WriteLine(beverage.GetDescription()); //Decaf Coffee, Mocha, Whip
+            Console.WriteLine(beverage.Cost()); // 1.35M
+
         }
     }
 }
