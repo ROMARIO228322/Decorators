caa8d57a7bbda00afe7933a0a080b7bdef3dcb0d
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 16:58:08 +0200

    Create Decaf.cs in Coffees folder

---
+++ DecoratorAssignment/Coffees/Decaf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.BeveragesList
+{
+    public class Decaf : Beverage
+    {
+        public Decaf()
+        {
+            description = "Decaf Coffee";
+        }
+        public override decimal Cost()
+        {
+            return 1.05M;
+        }
+    }
+}
