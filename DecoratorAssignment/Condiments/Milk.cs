e8df97f9ab66485dff5dd8f8ae85994f044cccbe
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:04:11 +0200

    Create Milk.cs in Condiments Folder

---
+++ DecoratorAssignment/Condiments/Milk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.CondimentsList
+{
+    public class Milk : CondimentsDecorator
+    {
+        Beverage beverage;
+        public Milk( Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+        public override decimal Cost()
+        {
+            return 0.30M+ beverage.Cost();
+        }
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", Milk";
+        }
+    }
+}
