c03e6c5e0f87e9155c38e8320af4123b4fdc8a8c
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 16:56:29 +0200

    Create Dark roast.cs in Coffees Folder

---
+++ DecoratorAssignment/Coffees/Dark roast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.BeveragesList
+{
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            description = "Dark Roast Coffee";
+        }
+        public override decimal Cost()
+        {
+            return 0.99M;
+        }
+    }
+}
