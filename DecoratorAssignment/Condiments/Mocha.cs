f45bf802ee0bd139208203bcb2415a2ccaafeaf3
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:04:52 +0200

    Create Mocha.cs in Condiments Folder

---
+++ DecoratorAssignment/Condiments/Mocha.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.CondimentsList
+{
+    public class Mocha : CondimentsDecorator
+    {
+        Beverage beverage;
+
+        public Mocha(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+        public override decimal Cost()
+        {
+            return 0.20M + beverage.Cost();
+        }
+
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", Mocha";
+        }
+    }
+}
