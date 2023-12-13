71725f68051af62c5372c9629adfb8a30518d1c3
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 16:55:37 +0200

    Create House blend.cs in Coffees Folder

---
+++ DecoratorAssignment/Coffees/House blend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.BeveragesList
+{
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            description = "House Blend Coffee";
+        }
+        public override decimal Cost()
+        {
+            return 0.89M;
+        }
+    }
+}
