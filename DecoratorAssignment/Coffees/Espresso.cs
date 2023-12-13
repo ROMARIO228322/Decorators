40dbc6589639b3d057fafb9e65212df94bb670ac
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 16:58:47 +0200

    Create Espresso.cs in Coffees Folder

---
+++ DecoratorAssignment/Coffees/Espresso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.BeveragesList
+{
+    public class Espresso : Beverage
+    {
+       public Espresso()
+        {
+            description = "Espresso";
+        }
+        public override decimal Cost()
+        {
+            return 1.99M;
+        }
+    }
+}
