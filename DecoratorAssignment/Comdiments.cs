62a0b0b2da234402f1795d0a785cb7b395ae07f1
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:22:56 +0200

    Create Comdiments.cs
    Add Condiments.cs

---
+++ DecoratorAssignment/Comdiments.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment
+{
+    public abstract class Condiments : Beverage
+    {
+       // while inheriting the beverages it also inherits the GetDescription and Cost method that's why didn't added here those methods.
+    }
+}
