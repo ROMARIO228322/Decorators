631bd7af6ad7f1709fe2f438ce75ab45640261c8
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:24:37 +0200

    Create Beverage.cs
    Add Beverage.cs

---
+++ DecoratorAssignment/Beverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment
+{
+    public abstract class Beverage
+    {
+        protected string description = "Unknown Beverage";
+        public virtual string GetDescription()
+        {
+            return description;
+        }
+        public abstract decimal Cost();
+    }
+}
