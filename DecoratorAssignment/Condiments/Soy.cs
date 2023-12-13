ec635209d33e34d0524bbaa082e2bfe945272e4c
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:05:39 +0200

    Create Soy.cs in Condiments Folder

---
+++ DecoratorAssignment/Condiments/Soy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment.CondimentsList
+{
+    public class Soy : CondimentsDecorator
+    {
+        Beverage beverage;
+
+        public Soy(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", Soy";
+        }
+
+        public override decimal Cost()
+        {
+            return 0.15M + beverage.Cost();
+        }
+    }
+}
